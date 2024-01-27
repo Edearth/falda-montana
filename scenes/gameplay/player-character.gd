d0d771d64e2899ee99937fea4543e48e747424da
Antimundo21 <pablo.rubio.ru@gmail.com>
2024-01-27 16:38:47 +0100

    Walls bounce less

--- scenes/gameplay/player-character.gd
+++ scenes/gameplay/player-character.gd
@@ -11,6 +11,10 @@ func _physics_process(delta):
 	if collision:
 		var bounce_magnitude = velocity.bounce(collision.get_normal()).length()
 		var lerped_bounce = lerpf(max_bounce, min_bounce, bounce_magnitude / 100)
+		if (collision.get_collider() as Node).is_in_group("walls"):
+			lerped_bounce = .5
 		velocity = velocity.bounce(collision.get_normal()) * lerped_bounce
+		
+			
 		if collision.get_collider().has_method("hit"):
 			collision.get_collider().hit()
