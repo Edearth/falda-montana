74f91dbfff6a47c5ac6c69f4b50854061d9ec67d
Antimundo21 <pablo.rubio.ru@gmail.com>
2024-01-27 18:55:55 +0100

    Tilemap and game beginning

--- scenes/gameplay/camera.gd
+++ scenes/gameplay/camera.gd
@@ -5,3 +5,5 @@ extends Camera2D
 func _process(delta):
 	if player_character.get_position().y < position.y - 100:
 		position.y = player_character.get_position().y + 100
+	elif player_character.get_position().y > position.y + 300:
+		position.y = player_character.get_position().y - 300
--- scenes/gameplay/player-character.gd
+++ scenes/gameplay/player-character.gd
@@ -9,15 +9,24 @@ var speed: float = 10
 func _physics_process(delta):
 	velocity.y += delta * gravity
 	var collision = move_and_collide(velocity * delta * speed)
+	print(velocity.y)
 	if collision:
 		var bounce_magnitude = velocity.bounce(collision.get_normal()).length()
 		var lerped_bounce = lerpf(max_bounce, min_bounce, bounce_magnitude / 100)
+		if (collision.get_collider() as Node).is_in_group("bottom_floor"):
+			lerped_bounce = 1
+			if velocity.y > 100:
+				lerped_bounce = .5
+			elif velocity.y < 30:
+				lerped_bounce = 2
+			
 		if (collision.get_collider() as Node).is_in_group("walls"):
 			lerped_bounce = .5
 		velocity = velocity.bounce(collision.get_normal()) * lerped_bounce
 		update_sprites()
 		if collision.get_collider().has_method("hit"):
 			collision.get_collider().hit()
+		
 	if velocity.y > 0:
 		$Sprite2D.set_fall_sprite(velocity.y)
 
