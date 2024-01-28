62d6bea6fe829e1b8336c5a11862ced0babb5d38
Antimundo21 <pablo.rubio.ru@gmail.com>
2024-01-28 13:55:28 +0100

    Add joke pickup sound

--- scenes/gameplay/joke.gd
+++ scenes/gameplay/joke.gd
@@ -6,5 +6,6 @@ func _on_area_2d_body_entered(body):
 		$Area2D.queue_free()
 		$AnimationPlayer.play("pickup")
 		$CPUParticles2D.set_emitting(true)
+		$AudioStreamPlayer.play()
 		await get_tree().create_timer(1).timeout
 		queue_free()
