90db313065e280b7f7952aaddd80e267cd5b08c1
Edearth <nepormoseno1992@gmail.com>
2024-01-28 06:42:18 +0100

    Add warning helpers and fix positions

--- scenes/gameplay/bushes/bush_controller.gd
+++ scenes/gameplay/bushes/bush_controller.gd
@@ -1,4 +1,11 @@
+@tool
 extends StaticBody2D
+class_name BushController
+
+func _get_configuration_warnings():
+	if not get_parent() is BushManager:
+		return ["Este nodo espera un parent de tipo BushManager. No hace nada si no tiene parent."]
+	return []
 
 func hit(_position: Vector2, _normal: Vector2):
 	get_parent().hit()
--- scenes/gameplay/bushes/bush_manager.gd
+++ scenes/gameplay/bushes/bush_manager.gd
@@ -1,5 +1,12 @@
+@tool
 extends Node2D
+class_name BushManager
 
+func _get_configuration_warnings():
+	for child in get_children():
+		if child is BushController:
+			return []
+	return ["Este nodo espera que que tengas nodos hijos de tipo Bush. No hace nada si está vacío"]
 
 func hit():
 	queue_free()
