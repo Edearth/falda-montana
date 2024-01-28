c262b38fe2b2ef974cfa16453b04f814f4f9a5fb
Edearth <nepormoseno1992@gmail.com>
2024-01-28 03:56:22 +0100

    Rock warning particles

--- scenes/gameplay/rock/rock_controller.gd
+++ scenes/gameplay/rock/rock_controller.gd
@@ -3,6 +3,13 @@ extends RigidBody2D
 
 func _ready():
 	angular_velocity = randf_range(-30, 30)
+	var warning: CPUParticles2D = $WarningParticles
+	warning.restart()
+	warning.reparent(get_tree().root)
+	freeze = true
+	var launch_rock = get_tree().create_timer(2)
+	launch_rock.timeout.connect(set_freeze_enabled.bind(false))
+	warning.finished.connect(warning.queue_free)
 
 func _physics_process(delta):
 	var bodies = get_colliding_bodies()
