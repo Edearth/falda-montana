e8731e4cfc5f7834ea042f0d9ea2f3be3bf95a65
Antimundo21 <pablo.rubio.ru@gmail.com>
2024-01-28 12:17:20 +0100

    Add joke feedback

--- scenes/gameplay/joke.gd
+++ scenes/gameplay/joke.gd
@@ -3,4 +3,8 @@ extends Node2D
 func _on_area_2d_body_entered(body):
 	if body is CharacterBody2D:
 		Global.jokes_collected += 1
+		$Area2D.queue_free()
+		$AnimationPlayer.play("pickup")
+		$CPUParticles2D.set_emitting(true)
+		await get_tree().create_timer(1).timeout
 		queue_free()
