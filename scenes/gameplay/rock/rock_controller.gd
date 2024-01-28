745053d97b0d63dcc093bfc3d8cf99381c2913fa
Edearth <nepormoseno1992@gmail.com>
2024-01-28 12:24:14 +0100

    Fix rocks frozen in space

--- scenes/gameplay/rock/rock_controller.gd
+++ scenes/gameplay/rock/rock_controller.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+signal rock_starts_falling
 signal rock_destroyed
 
 @export var TIME_TO_START_FALLING: float = 2.0
@@ -11,8 +12,12 @@ func _ready():
 	warning.call_deferred("reparent", get_tree().root)
 	freeze = true
 	var launch_rock = get_tree().create_timer(TIME_TO_START_FALLING)
-	launch_rock.timeout.connect(set_freeze_enabled.bind(false))
+	launch_rock.timeout.connect(do_launch_rock)
 	warning.finished.connect(warning.queue_free)
+
+func do_launch_rock():
+	set_freeze_enabled(false)
+	rock_starts_falling.emit()
 
 func _physics_process(delta):
 	var bodies = get_colliding_bodies()
