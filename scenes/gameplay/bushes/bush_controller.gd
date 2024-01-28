e5f780bdbf099d4fccf30de93bb8dbb2ef6fe1f4
Antimundo21 <pablo.rubio.ru@gmail.com>
2024-01-28 11:15:52 +0100

    Add bush feedback

--- scenes/gameplay/bushes/bush_controller.gd
+++ scenes/gameplay/bushes/bush_controller.gd
@@ -2,10 +2,18 @@
 extends StaticBody2D
 class_name BushController
 
+var has_been_hit = false
+
 func _get_configuration_warnings():
 	if not get_parent() is BushManager:
 		return ["Este nodo espera un parent de tipo BushManager. No hace nada si no tiene parent."]
 	return []
 
 func hit(_position: Vector2, _normal: Vector2):
+	$AnimationPlayer.play("hit")
+	has_been_hit = true
 	get_parent().hit()
+
+func hit_indirect():
+	if not has_been_hit:
+		$AnimationPlayer.play("hit_indirect")
--- scenes/gameplay/bushes/bush_manager.gd
+++ scenes/gameplay/bushes/bush_manager.gd
@@ -9,4 +9,8 @@ func _get_configuration_warnings():
 	return ["Este nodo espera que que tengas nodos hijos de tipo Bush. No hace nada si está vacío"]
 
 func hit():
+	for child in get_children():
+		if child.has_method("hit_indirect"):
+			child.hit_indirect()
+	await get_tree().create_timer(.3).timeout
 	queue_free()
